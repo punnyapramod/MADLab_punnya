39f210fcd3243a167fc25eb6b1d99e1decbb5692
punnyapramod <punnyapramod4@gmail.com>
2025-10-08 12:38:08 +0530

    Create privacyandpolicy.java

---
+++ OPTION MENU/JAVA/privacyandpolicy.java
@@ -0,0 +1,15 @@
+
+package com.example.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class privacypolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacypolicy);
+    }
+}
