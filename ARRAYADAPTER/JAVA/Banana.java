e2c1c9c394d71b397531cf5b08a5a38999088104
punnyapramod <punnyapramod4@gmail.com>
2025-10-13 12:43:05 +0530

    Create Banana.java

---
+++ ARRAYADAPTER/JAVA/Banana.java
@@ -0,0 +1,15 @@
+package com.example.arrayadapter;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Banana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banana);
+    }
+}
